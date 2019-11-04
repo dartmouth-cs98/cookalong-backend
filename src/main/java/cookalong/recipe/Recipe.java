bb633b4f208904e0782213f36a8bd7f499468348
Brian Tomasco <brian.a.tomasco.20@dartmouth.edu>
2019-11-03 21:48:30 -0500

    Added data model for recipe and endpoint to retrieve a default

---
+++ src/main/java/cookalong/recipe/Recipe.java
@@ -0,0 +1,81 @@
+package cookalong.recipe;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Recipe {
+
+    /*
+    id,
+    name,
+    dish_description,
+    image_url,
+    time,
+    serving_size,
+    calories,
+    ingredients (name, amount, allergens),
+    steps: list of step.id,
+    tools,
+    techniques,
+    tags,
+    */
+    private long id;
+    private String name;
+    private String description;
+    private int time;
+    private int serving_size;
+    private int calories;
+    private List<Integer> ingredients;
+    private List<Integer> steps;
+    private List<String> tools;
+//    private List<Integer> techniques;
+//    private List<Integer> tags;
+
+    public Recipe(long id, String name, String description, int time, int serving_size, int calories) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.time = time;
+        this.serving_size = serving_size;
+        this.calories = calories;
+        this.ingredients = new ArrayList<>();
+        this.tools = new ArrayList<>();
+        this.steps = new ArrayList<>();
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public int getServing_size() {
+        return serving_size;
+    }
+
+    public int getCalories() {
+        return calories;
+    }
+
+    public List<Integer> getIngredients() {
+        return ingredients;
+    }
+
+    public List<Integer> getSteps() {
+        return steps;
+    }
+
+    public List<String> getTools() {
+        return tools;
+    }
+}
---
+++ src/main/java/cookalong/recipe/RecipeController.java
@@ -0,0 +1,25 @@
+package cookalong.recipe;
+
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RecipeController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/recipe")
+    public Recipe recipe(@RequestParam(value="name", defaultValue="Grilled Cheese") String name) {
+        return new Recipe(
+                counter.incrementAndGet(),
+                name,
+                "Melted cheese on toasted bread",
+                10,
+                1,
+                280
+                );
+    }
+}
