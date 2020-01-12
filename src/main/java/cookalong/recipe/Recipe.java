7ea7e1bd6a15e31f2e5bb34048a6b0df6c1b4231
Brian Tomasco <brian.a.tomasco.20@dartmouth.edu>
2020-01-12 16:19:20 -0500

    Did preliminary setup for MongoDB

--- src/main/java/cookalong/recipe/Recipe.java
+++ src/main/java/cookalong/recipe/Recipe.java
@@ -3,8 +3,13 @@ package cookalong.recipe;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
 public class Recipe {
 
+    @Id
     private long id;
     private String name;
     private String description;
